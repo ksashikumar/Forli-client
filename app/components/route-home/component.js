d006ca66a4a47c239309674df860e2f93d642fb6
Vimal-Ramakrishnan <vimal@freshdesk.com>
2017-07-18 23:51:16 +0530

    (Api Setup)
    Basic API setup has been implemented

--- app/components/route-home/component.js
+++ app/components/route-home/component.js
@@ -2,7 +2,14 @@ import Ember from 'ember';
 
 const {
   Component,
+  inject
 } = Ember;
 
 export default Component.extend({
+  store: inject.service(),
+  init() {
+    this._super(...arguments);
+    let testData = this.get('store').findAll('discussion');
+    console.log('testData', testData);
+  }
 });
