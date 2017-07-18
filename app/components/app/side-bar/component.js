16763660380b19670934337d9dce469ec609e25d
shibulijack <shibulijack@gmail.com>
2017-07-18 15:08:50 +0530

    enhance(sidebar): updated design

--- app/components/app/side-bar/component.js
+++ app/components/app/side-bar/component.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    // Fetch categories and tags
+    categories: [{"id":"1", "name":"Programming"},{"id":"2", "name":"Design"},{"id":"2", "name":"Development"}],
+    tags: [{"id":"1", "name":"Javascript"},{"id":"2", "name":"Ember"},{"id":"2", "name":"HTML"}],
+
+    actions: {
+    askQuestion() {
+      alert('Perform ask');
+    }
+  }
 });
