2211b2ce80570c00cd8c7fee055baa22b40271f0
shibulijack <shibulijack@gmail.com>
2017-07-19 02:25:23 +0530

    refactor(app): css & template changes

--- app/components/route-home/forum-list/component.js
+++ app/components/route-home/forum-list/component.js
@@ -6,6 +6,6 @@ const {
 
 export default Component.extend({
   descriptionTruncateLimit: 170,
-  tagList: ['veri', 'veri', 'sathipan'],
+  tagList: ['JavaScript', 'HTML', 'CSS'],
   desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled",
 });
