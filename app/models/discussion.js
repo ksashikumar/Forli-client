abb402bf65b22eeb506cbc2901aa9152f0286652
Vimal-Ramakrishnan <vimal@freshdesk.com>
2017-07-20 21:37:19 +0530

    Discussion Creation basic setup

--- app/models/discussion.js
+++ app/models/discussion.js
@@ -4,5 +4,7 @@ import attr from 'ember-data/attr';
 export default DS.Model.extend({
   title: attr('string'),
   description: attr('string'),
-  user: attr()
+  user: attr(),
+  tags: attr(),
+  categoryId: attr()
 });
