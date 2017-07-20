8d741ba932664370c82dcf1e07bd54376e32ed17
Vimal-Ramakrishnan <vimal@freshdesk.com>
2017-07-20 21:05:03 +0530

    Added global spinner for when ember is setting up.

--- app/components/app/avatar-holder/component.js
+++ app/components/app/avatar-holder/component.js
@@ -9,7 +9,6 @@ export default Component.extend({
   tagName: '',
   initial: computed('name', function() {
     let userName = this.get('name');
-    console.log('userName', userName);
     return userName.trim().charAt(0);
   }),
 });
