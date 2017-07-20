739aab2f39c44ca2774c8483036dd9af5436ebef
shibulijack <shibulijack@gmail.com>
2017-07-20 19:20:48 +0530

    enhance(sidebar): style update

--- app/components/app/side-bar/component.js
+++ app/components/app/side-bar/component.js
@@ -2,8 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     // Fetch categories and tags
-    categories: [{"id":"1", "name":"Programming"},{"id":"2", "name":"Design"},{"id":"2", "name":"Development"}],
-    tags: [{"id":"1", "name":"Javascript"},{"id":"2", "name":"Ember"},{"id":"2", "name":"HTML"}],
+    categories: [{"id":"1", "name":"Programming"},{"id":"2", "name":"Design"},{"id":"3", "name":"Development"},{"id":"4", "name":"Testing"},{"id":"5", "name":"Devops"}],
+    tags: [{"id":"1", "name":"Javascript"},{"id":"2", "name":"Ember"},{"id":"3", "name":"HTML"},{"id":"4", "name":"Angular"},{"id":"5", "name":"PHP"}],
 
     actions: {
     askQuestion() {
