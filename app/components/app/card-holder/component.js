07508d4840689767a604eeacbe2249f1c8622944
shibulijack <shibulijack@gmail.com>
2017-07-23 02:05:21 +0530

    refactor(discussion): code cleanup

--- app/components/app/card-holder/component.js
+++ app/components/app/card-holder/component.js
@@ -7,6 +7,6 @@ const {
 export default Ember.Component.extend({
     formattedTag: computed('discussion.tags', function() {
         let tags = this.get('discussion.tags');
-        return tags.length > 0 ? 'Also Tagged as "'+tags[0].name +'"' : "Related Post";
+        return tags.length > 0 ? 'Also Tagged as "'+tags[0].name +'"' : "Similar Questions";
     })
 });
