a10eb16805f8a003a0a56fdb9410e4f31e4dcbeb
Kanalveli <kaelin.ralav@gmail.com>
2017-07-23 21:18:34 +0530

    search drop down tweaks

--- app/components/app/quick-search/component.js
+++ app/components/app/quick-search/component.js
@@ -10,12 +10,20 @@ const {
 
 export default Component.extend(performSearch, {
   term: '',
-  showResults: false,
+  showResultsDropdown: true,
   isTermPresent: computed.gt('term.length', 0),
   showLinkToPage: computed.gt('searchResults.lastSuccessful.value.meta.count', searchConstant.searchLimit),
   searchResults: task(function* () {
     yield timeout(searchConstant.debounceTime);
     let result = yield this.performSearch({ term: this.get('term') });
     return result;
-  }).restartable()
+  }).restartable(),
+  click() {
+    this.toggleProperty('showResultsDropdown');
+    this.set('term', '');
+  },
+  didInsertElement() {
+    document.querySelector('#quick-search-input').focus();
+    this._super(...arguments);
+  }
 });
