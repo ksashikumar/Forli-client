9a80a59a7d768cbe21b9ac5614ce80c4e954d435
Vimal-Ramakrishnan <vimal@freshdesk.com>
2017-07-23 15:47:23 +0530

    Comments section basic stub data impl/N

--- app/components/route-discussion/component.js
+++ app/components/route-discussion/component.js
@@ -9,7 +9,51 @@ const upvoteActionState = 2;
 const downvoteActionState = 1;
 
 export default Ember.Component.extend({
+  content: null,
   relatedQuestions: [],
+  globalReplyBox: true,
+  answers: [
+    {
+      answer: {
+        content: "1-asdfasdf asdfkjasdf asdfaksdfnasdkfjasdkf nasdfkjna sfdkjna sdf adfskna dfkn",
+        user: {
+          userId:1,
+          name: 'Jack Daniels',
+          image: 'http://garden.zendesk.com/css-avatars/images/jz.png',
+          email: 'jack@forli.com' },
+      },
+      replies: [{
+        content: "Reply1",
+        date: "Sun Jul 23 2017 14:49:19 GMT+0530 (IST)"
+      },{
+        content: "Reply2",
+        date: "Sun Jul 23 2017 14:49:19 GMT+0530 (IST)"
+      }, {
+        content: "Reply3",
+        date: "Sun Jul 23 2017 14:49:19 GMT+0530 (IST)"
+      }]
+    },
+    {
+      answer: {
+        content: "2-asdfasdf asdfkjasdf asdfaksdfnasdkfjasdkf nasdfkjna sfdkjna sdf adfskna dfkn",
+        user: {
+          userId:1,
+          name: 'Jack Daniels',
+          image: 'http://garden.zendesk.com/css-avatars/images/jz.png',
+          email: 'jack@forli.com' },
+      },
+      replies: [{
+        content: "Reply1",
+        date: "Sun Jul 23 2017 14:49:19 GMT+0530 (IST)"
+      },{
+        content: "Reply2",
+        date: "Sun Jul 23 2017 14:49:19 GMT+0530 (IST)"
+      }, {
+        content: "Reply3",
+        date: "Sun Jul 23 2017 14:49:19 GMT+0530 (IST)"
+      }]
+    },
+  ],
   votes: computed('discussion.upvotesCount', 'discussion.downvotesCount', function() {
     return get(this, 'discussion.upvotesCount') - get(this, 'discussion.downvotesCount');
   }),
@@ -39,6 +83,19 @@ export default Ember.Component.extend({
     this.set('discussion.voteAction', actionState);
   },
   actions: {
+    submitAnswer(comment) {
+      let currentDiscussion = this.get('discussion');
+      currentDiscussion.answer({
+        "content": comment
+      });
+    },
+    submitReply(reply, answerId) {
+      let currentDiscussion = this.get('discussion');
+      currentDiscussion.reply({
+        "content": reply,
+        "answer_id": answerId
+      });
+    },
     upVote() {
       let currentDiscussion = this.get('discussion');
       currentDiscussion.upvote().then(() => {
@@ -52,6 +109,9 @@ export default Ember.Component.extend({
         this.incrementProperty('discussion.downvotesCount');
         this.handlePreviousVoteState(downvoteActionState);
       })
-    }
+    },
+    showGlobalReply() {
+      this.toggleProperty('globalReplyBox');
+    },
   }
 });
