25bc47f1198f095226dfa5be495b79fc683bab4d
Kanalveli <kaelin.ralav@gmail.com>
2017-07-23 17:26:32 +0530

    View All results

--- app/components/app/quick-search/component.js
+++ app/components/app/quick-search/component.js
@@ -1,19 +1,21 @@
 import Ember from 'ember';
 import performSearch from 'forli/mixins/perform-search';
 import { task, timeout } from 'ember-concurrency';
+import searchConstant from 'forli/constants/search';
 
 const {
   get,
   Component,
   computed
 } = Ember;
+
 export default Component.extend(performSearch, {
   term: '',
   isTermPresent: computed.gt('term.length', 1),
-  isLoading: computed.reads('searchResults.isPending'),
+  showLinkToPage: computed.gt('searchResults.lastSuccessful.value.meta.count', searchConstant.searchLimit),
   searchResults: task(function* () {
     if(get(this, 'isTermPresent')) {
-      yield timeout(500);
+      yield timeout(searchConstant.debounceTime);
       let result = yield this.performSearch({ term: this.get('term') });
       return result;
     }
---
+++ app/constants/search.js
@@ -0,0 +1,7 @@
+
+const searchConstant = {
+  debounceTime: 300,
+  searchLimit: 10
+};
+
+export default searchConstant;
