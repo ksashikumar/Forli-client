4248ad5b3b5ddd8d1765a579d3864192374abe78
Kanalveli <kaelin.ralav@gmail.com>
2017-07-23 20:11:34 +0530

    (enhance:comment-holder) Upvotes down vote)

--- app/models/answer.js
+++ app/models/answer.js
@@ -1,7 +1,15 @@
 import DS from 'ember-data';
+import { memberAction } from 'ember-api-actions';
+import { belongsTo } from 'ember-data/relationships';
 import attr from 'ember-data/attr';
 
 export default DS.Model.extend({
+  discussion: belongsTo('discussion'),
   content: attr(),
   discussionId: attr(),
+  downvotesCount: attr(),
+  upvotesCount: attr(),
+  voteAction: attr(),
+  upvote: memberAction({ path: 'upvote' }),
+  downvote: memberAction({ path: 'downvote' }),
 });
