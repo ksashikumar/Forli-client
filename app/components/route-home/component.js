36665f097e8c653272020669937a3b8427247552
Vimal-Ramakrishnan <vimal@freshdesk.com>
2017-07-28 22:57:20 +0530

    Shimmer-Implemented

--- app/components/route-home/component.js
+++ app/components/route-home/component.js
@@ -11,14 +11,35 @@ const perPage = 10;
 
 export default Component.extend({
   perPage,
+  shimmer: false,
   store: service(),
   meta: {},
   loadedCount: computed.reads('discussion.length'),
   totalCount: computed.reads('meta.count'),
   paramsListener: observer('params', function() {
     //  Setting page index to 1, Whenever we are getting new params,
+    this.toggleProperty('shimmer');
     this.set('page', 1);
+    this.loadDiscussion();
   }),
+  init() {
+    this._super(...arguments);
+    this.loadDiscussion();
+  },
+  loadDiscussion() {
+    let params = this.get('params');
+    let paginatedData = this.get('store').query('discussion', {
+      tag_ids: params.tag_ids,
+      filter: params.filter
+    });
+    paginatedData.then((result) => {
+      this.toggleProperty('shimmer');
+      this.set('discussion', result);
+      this.set('meta', result.get('meta'))
+    }).catch(()=>{
+      // catch Error
+    });
+  },
   page: 1,
   actions: {
     loadMore() {
--- app/routes/home.js
+++ app/routes/home.js
@@ -16,17 +16,25 @@ export default Route.extend({
       replace: true
     },
   },
+  // model(params) {
+  //   // Since home page moved to separate route
+  //   // We are linked home page params to service (refer service -> forli.params)
+  //   this.set('forli.params', params);
+  //
+  //   return RSVP.hash({
+  //     params: params,
+  //     discussion: this.get('store').query('discussion', {
+  //       tag_ids: params.tag_ids,
+  //       filter: params.filter
+  //     })
+  //   });
+  // },
   model(params) {
     // Since home page moved to separate route
     // We are linked home page params to service (refer service -> forli.params)
     this.set('forli.params', params);
-
-    return RSVP.hash({
-      params: params,
-      discussion: this.get('store').query('discussion', {
-        tag_ids: params.tag_ids,
-        filter: params.filter
-      })
-    });
+    return {
+      params: params
+    }
   },
 });
