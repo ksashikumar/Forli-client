29d63274e24740169cad83fd7d3ea6a07a406209
K Sashi Kumar <ksashikumark93@gmail.com>
2017-08-01 02:14:05 +0530

    Change auth url config

--- app/authenticators/devise.js
+++ app/authenticators/devise.js
@@ -2,8 +2,11 @@ import Ember from 'ember';
 import Devise from 'ember-simple-auth/authenticators/devise';
 
 export default Devise.extend({
-  serverTokenEndpoint: 'https://forli-api-dev.herokuapp.com/api/v1/sign_in',
-  serverLogoutEndpoint: 'https://forli-api-dev.herokuapp.com/api/v1/sign_out',
+  serverTokenEndpoint: 'https://forli-api.herokuapp.com/api/v1/sign_in',
+  serverLogoutEndpoint: 'https://forli-api.herokuapp.com/api/v1/sign_out',
+  // serverTokenEndpoint: 'http://localhost:3000/api/v1/sign_in',
+  // serverLogoutEndpoint: 'http://localhost:3000/api/v1/sign_out',
+
   invalidate: function() {
     return Ember.$.ajax({
       url:  this.serverLogoutEndpoint,
